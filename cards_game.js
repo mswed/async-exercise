5579f0c4577acccf030d496fb7a93b7bd65fa400
Moshe Swed <mswed@beapot.com>
2024-05-28 22:48:45 -0400

    Refactored part 2

--- cards_game.js
+++ cards_game.js
@@ -1,37 +1,71 @@
 const hitMeBtn = document.querySelector('button')
 const cards = document.querySelector('div.cards')
 const baseURL = 'https://deckofcardsapi.com/api/deck'
+let endGame = false;
 
 let deck = ''
 
+class Deck {
+    constructor() {
+        this.deckID = null;
+        this.newShuffle()
+    }
 
-function newDeck() {
-    return axios.get(`${baseURL}/new/shuffle/`, {params: {deck_count: 1}})
+    async newShuffle() {
+        // Create a new deck and shuffle it (the api has a call for that, but I'll do it here instead
+
+        await this.new()
+        this.shuffle()
+    }
+    async new() {
+        const res = await axios.get(`${baseURL}/new/`)
+        this.deckID = res.data.deck_id
+
+    }
+
+    async shuffle() {
+        const res = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckID}/shuffle/`)
+        console.log('Shuffled the deck!')
+    }
+    async drawCard() {
+        try {
+            const res = await axios.get(`${baseURL}/${this.deckID}/draw`, {params: {count: 1}})
+            const {remaining} = res.data
+            const {suit, value, image} = res.data.cards[0]
+            console.log(remaining)
+            return {suit, value, image, remaining}
+        } catch (e) {
+            console.log(e)
+        }
+
+
+    }
+
+
 }
 
-function drawCard(deck) {
-    return axios.get(`${baseURL}/${deck}/draw`, {params: {count: 1}})
-}
+hitMeBtn.addEventListener('click', async (e) => {
+    if (endGame) {
+        alert('Out of cards! Refresh the page to play again')
+        return
+    }
 
-hitMeBtn.addEventListener('click', (e) => {
-    drawCard(deck).then(res => {
-        const card = document.createElement('img')
-        card.src = res.data.cards[0].image
-        card.className = 'card'
-        const angle = Math.floor(Math.random() * 20)
+    const card = await deck.drawCard()
+    const cardElement = document.createElement('img')
+    cardElement.src = card.image
+    cardElement.className = 'card'
+    const angle = Math.floor(Math.random() * 20)
 
-        card.style.rotate = `${angle}deg`
-        cards.append(card)
+    cardElement.style.rotate = `${angle}deg`
+    cards.append(cardElement)
+    if (card.remaining === 0) {
+        endGame = true;
 
-    })
+    }
 })
 
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    newDeck()
-        .then(res => {
-            deck = res.data.deck_id
-        })
-        .catch(err => console.log(err))
+    deck = new Deck()
 })
