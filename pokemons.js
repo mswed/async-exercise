d1a8cdc277b7c2260f36cdfdc373910a5df74d2f
Moshe Swed <mswed@beapot.com>
2024-05-30 21:57:39 -0400

    further study refactored

--- pokemons.js
+++ pokemons.js
@@ -1,116 +1,112 @@
 const baseURL = 'https://pokeapi.co/api/v2/pokemon'
 const getPokeBtn = document.querySelector('button')
 const drawnCards = document.getElementById('drawnCards')
+let game = null;
 
-function getAll() {
-    return axios.get(`${baseURL}`, {params: {limit: 1302}})
-}
+class PokemonGame {
+    constructor() {
+        this.totalPokemons = null;
+        this.allPokemons = null;
+        this.selectedPokemonDetails = [];
+        this.pokemons = {};
+        this.getAllPoke()
 
-const selectedPokemons = []
-
-function buildCard(name, image, text) {
-    const column = document.createElement('div')
-    column.className = 'col-4'
-    const card = document.createElement('div')
-    card.className = 'card'
-    const cardImage = document.createElement('img')
-    cardImage.src = image
-    cardImage.style = 'width: 100%'
-    const container = document.createElement('div')
-    container.className = 'container'
-    const cardText = document.createElement('div')
-    cardText.innerHTML = `<h4><b>${name}</b></h4>
-                    <p>${text}</p>`
-
-    column.append(card)
-    card.append(cardImage)
-    card.append(container)
-    container.append(cardText)
-    drawnCards.append(column)
-
-}
-function drawPokemons() {
-    drawnCards.innerHTML = ''
-    getAll().then(res => {
-        const selection = selectRandomPoke(res.data.results)
-
-        Promise.all(selection)
-            .then(pokemons => {
-                for (poke of pokemons) {
-                    const name = poke.data.name;
-                    const image = poke.data.sprites.front_default
-                    axios.get(`https://pokeapi.co/api/v2/pokemon-species/${poke.data.species.name}/`)
-                        .then(res => {
-                            for (fte of res.data.flavor_text_entries) {
-                                if (fte.language.name === 'en') {
-                                    const species_text = fte.flavor_text
-                                    buildCard(name, image, species_text)
-                                    return
-                                }
-                            }
-                            buildCard(name, image, 'No english description found')
-                        })
-                }
-            })
-            .catch(err => console.log(err))
-    })
-}
-
-function selectRandomPoke(pokemons) {
-    const selectedPokemons = []
-    for (let i = 0; i < 3; i++) {
-        const selectedPokemon = pokemons[Math.floor(Math.random() * 1302)]
-        selectedPokemons.push(axios.get(selectedPokemon.url))
     }
 
-    return selectedPokemons;
-}
+    async draw() {
+        this.selectedPokemonDetails = [];
+        this.pokemons = {};
+        await this.selectRandomPoke()
+        await this.buildPokemon()
+        await this.getSpeciesDetails()
 
-function getSpeciesText(species_name, name) {
-    const species = axios.get(`https://pokeapi.co/api/v2/pokemon-species/${species_name}/`)
-        .then(res => {
-            for (fte of res.data.flavor_text_entries) {
-                if (fte.language.name === 'en') {
-                    return [name, fte.flavor_text]
-                } else {
-                    return [name, null]
-                }
+        console.log(this.pokemons)
+        for (const p in this.pokemons) {
+            if (this.pokemons.hasOwnProperty(p)) {
+                console.log(`${p}: ${this.pokemons[p].species}`)
+                this.buildCard(p, this.pokemons[p].image, this.pokemons[p].species)
             }
-        })
-}
+        }
+    }
+    async countPoke() {
+        const res = await axios.get(`${baseURL}`)
+        this.totalPokemons = res.data.count
+    }
 
-getAll()
-    .then(res => {
+    async getAllPoke() {
+        await this.countPoke()
+        const res = await axios.get(`${baseURL}`, {params: {limit: this.totalPokemons}})
+        this.allPokemons = res.data.results
+    }
+
+    async selectRandomPoke() {
+        const selectedPokemons = []
+        // Select three random pokemons
         for (let i = 0; i < 3; i++) {
-            const selectedPokemon = res.data.results[Math.floor(Math.random() * 1302)]
+            const selectedPokemon = this.allPokemons[Math.floor(Math.random() * this.totalPokemons)]
             selectedPokemons.push(axios.get(selectedPokemon.url))
         }
-        Promise.all(selectedPokemons)
-            .then(pokemons => {
-                for (poke of pokemons) {
-                    const name = poke.data.name
-                    const species = axios.get(`https://pokeapi.co/api/v2/pokemon-species/${poke.data.species.name}/`)
-                        .then(res => {
-                            for (fte of res.data.flavor_text_entries) {
-                                if (fte.language.name === 'en') {
-                                    console.log(`${name}: ${fte.flavor_text}`)
-                                    return
-                                }
-                            }
 
-                            console.log(name, 'No english description found')
-                        })
+        for (let p of selectedPokemons) {
+            const d = await p
+            this.selectedPokemonDetails.push(d.data)
+        }
+    }
+
+    buildPokemon() {
+        for (const p of this.selectedPokemonDetails) {
+            this.pokemons[p.name] = {'image': p.sprites.front_default, species: null}
+        }
+    }
+
+    buildCard(name, image, text) {
+        const column = document.createElement('div')
+        column.className = 'col-4'
+        const card = document.createElement('div')
+        card.className = 'card'
+        const cardImage = document.createElement('img')
+        cardImage.src = image
+        cardImage.style = 'width: 100%'
+        const container = document.createElement('div')
+        container.className = 'container'
+        const cardText = document.createElement('div')
+        cardText.innerHTML = `<h4><b>${name}</b></h4>
+                    <p>${text}</p>`
+
+        column.append(card)
+        card.append(cardImage)
+        card.append(container)
+        container.append(cardText)
+        drawnCards.append(column)
+    }
+
+    async getSpeciesDetails() {
+        const additionalDetails = [];
+        for (const p of this.selectedPokemonDetails) {
+            let speciesText = null;
+            const species = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${p.species.name}/`)
+            console.log(species)
+            for (const fte of species.data.flavor_text_entries) {
+                if (fte.language.name === 'en') {
+                    speciesText = fte.flavor_text
+                    console.log('Selected test', speciesText)
+                    break
+
                 }
-            })
-            .catch(err => console.log(err))
-    })
-    .catch(err => console.log(err))
 
-getPokeBtn.addEventListener('click', drawPokemons)
+            }
+            this.pokemons[p.name]['species'] = speciesText;
+        }
+    }
+}
+
+
+
+
+getPokeBtn.addEventListener('click', async () => {
+    drawnCards.innerHTML = ''
+    await game.draw()
+})
 document.addEventListener('DOMContentLoaded', function () {
-    getAll()
-        .then(res => {
-            deck = res.data.deck_id
-        })
-        .catch(err => console.log(err))
+    game = new PokemonGame()
 })
